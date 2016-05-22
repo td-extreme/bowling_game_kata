ceef06819af7705ec8f3ef0b1157a22bd47b0b0d
Tyler Decker <tyler.decker@gmail.com>
2016-05-22 08:52:06 -0500

    added steps

---
+++ steps/BowlingGameTest_2.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+  Game g;
+
+  protected void setUp() {
+    g = new Game();
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) 
+      g.roll(pins);
+  }
+
+  public void testGutterGame() throws Exception {
+    rollMany(20, 0); 
+    assertEquals(0, g.score());
+  }
+
+  public void testAllOnes() throws Exception {
+    rollMany(20, 1);
+    assertEquals(20, g.score());
+  }
+}
---
+++ steps/BowlingGameTest_3.java
@@ -0,0 +1,36 @@
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+  Game g;
+ 
+  protected void setUp() {
+    g = new Game();
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; ++i) 
+      g.roll(pins);
+  }
+
+  public void testGutterGame() throws Exception {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+
+  public void testAllOnes() throws Exception {
+    rollMany(20, 1);
+    assertEquals(20, g.score());
+  }
+
+  public void testOneSpare() throws Exception {
+    rollSpare();
+    g.roll(3);
+    rollMany(17, 0);
+    assertEquals(16, g.score());
+  }
+
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  } 
+}
